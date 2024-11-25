7e864d55e6a3be23d273e4285431cca8131fc2e4
Armanur Rashid <armanurrashid105086@gmail.com>
2024-11-26 00:06:36 +0600

    Updated About Sections Skill, Experience, Education

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,26 @@
-var typed = new Typed(".typing",{
-    strings: ["","Web Designer","Web Developer","Graphic Designer"],
+var typed = new Typed(".typing", {
+    strings: ["", "Web Designer", "Web Developer", "Graphic Designer"],
     typeSpeed: 100,
-    backSpeed:60,
-    loop:true
+    backSpeed: 60,
+    loop: true
 })
+
+
+window.addEventListener("DOMContentLoaded", () => {
+    const progressBars = document.querySelectorAll(".progress-in");
+
+    progressBars.forEach(bar => {
+        const target = parseInt(bar.getAttribute("data-target"), 10);
+        let width = 0;
+
+        const interval = setInterval(() => {
+            if (width >= target) {
+                clearInterval(interval);
+            } else {
+                width++;
+                bar.style.width = width + "%";
+                bar.nextElementSibling.textContent = width + "%"; 
+            }
+        }, 18);
+    });
+});
