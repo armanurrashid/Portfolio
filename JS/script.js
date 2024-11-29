8192d969944f85efee5a30ce8b18559a2b3f7875
Armanur Rashid <armanurrashid105086@gmail.com>
2024-11-30 00:11:33 +0600

    Sidebar Adjustment and Hire Me button issue solved

--- JS/script.js
+++ JS/script.js
@@ -1,64 +1,56 @@
 /* Active Link*/
-// const navLinks = document.querySelectorAll('.nav li a');
-// const allSection = document.querySelectorAll(".section");
-// const totalSection = allSection.length;
 
-// navLinks.forEach(link => {
-//     link.addEventListener('click', function () {
-//         navLinks.forEach(nav => {
-//             if (nav.classList.contains("active")) {
-//                 console.log(nav)
-//             }
-//             nav.classList.remove('active');
-//         });
-//         this.classList.add('active');
-//         showElement(this);
-//     });
-// });
 const nav = document.querySelector(".nav"),
     navList = nav.querySelectorAll("li"),
     totalNavList = navList.length,
     allSection = document.querySelectorAll(".section"),
     totalSection = allSection.length;
-    for(let i=0; i<totalNavList; i++){
-        const a = navList[i].querySelector("a");
-        a.addEventListener("click",function(){
-            for(let i=0; i<totalSection;i++){
-                allSection[i].classList.remove("back-section");
+for (let i = 0; i < totalNavList; i++) {
+    const a = navList[i].querySelector("a");
+    a.addEventListener("click", function () {
+       removeBackSection();
+        for (let j = 0; j < totalNavList; j++) {
+            if (navList[j].querySelector("a").classList.contains("active")) {
+                addBackSection(j);
             }
-            for(let j=0;j<totalNavList;j++){
-                if(navList[j].querySelector("a").classList.contains("active")){
-                    allSection[j].classList.add("back-section");
-                }
-                navList[j].querySelector("a").classList.remove("active");
-            }
-            this.classList.add("active")
-            showElement(this)
-        })
+            navList[j].querySelector("a").classList.remove("active");
+        }
+        this.classList.add("active")
+        showElement(this)
+    })
+}
+function removeBackSection(){
+    for (let i = 0; i < totalSection; i++) {
+        allSection[i].classList.remove("back-section");
     }
-
-// const navLinks = document.querySelectorAll('.nav li a');
-// const allSection = document.querySelectorAll(".section");
-// totalSection = allSection.length;
-// navLinks.forEach(link => {
-//     link.addEventListener('click', function () {
-//         if(this.querySelector("a") && this.querySelector("a").classList.contains("active")) {
-//             console.log(this.querySelector("a"));
-//         }
-//         navLinks.forEach(nav => nav.classList.remove('active'));
-//         this.classList.add('active');
-//         showElement(this)
-//     });
-// });
-
+}
+function addBackSection(num){
+    allSection[num].classList.add("back-section");
+}
 function showElement(element) {
-    for(let i=0; i<totalSection;i++){
+    for (let i = 0; i < totalSection; i++) {
         allSection[i].classList.remove("active");
     }
     const target = element.getAttribute("href").split("#")[1];
     document.querySelector("#" + target).classList.add("active")
 }
+function updateNav(element) {
+    for (let i = 0; i < totalNavList; i++) {
+        navList[i].querySelector("a").classList.remove("active");
+        const target = element.getAttribute("href").split("#")[1];
+        if (target === navList[i].querySelector("a").getAttribute("href").split("#")[1]) {
+            navList[i].querySelector("a").classList.add("active")
+        }
+    }
+}
 
+document.querySelector(".hire-me").addEventListener("click", function () {
+    const sectionIndex = this.getAttribute("data-section-index")
+    showElement(this)
+    updateNav(this);
+    removeBackSection();
+    addBackSection(sectionIndex);
+})
 var typed = new Typed(".typing", {
     strings: ["Web Designer", "Web Developer"],
     typeSpeed: 100,
